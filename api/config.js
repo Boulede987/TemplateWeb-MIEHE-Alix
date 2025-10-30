29c6f39511d09fc0e549fed7fd6c71de38b9b11f
MIEHE Alix <alixmiehe2004@gmail.com>
2025-10-30 15:50:46 +0100

    modif credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,12 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvchr0fns73cjb6i0-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "api_postgres_ryy1_user",
+    "password" : "yL2wyfp1sGIkKBRwPQIkl1b44MmXvj9z",
+    "bdname" :"api_postgres_ryy1" 
     }
 }
 
+// postgresql://api_postgres_ryy1_user:yL2wyfp1sGIkKBRwPQIkl1b44MmXvj9z@dpg-d41mvchr0fns73cjb6i0-a.oregon-postgres.render.com/api_postgres_ryy1
